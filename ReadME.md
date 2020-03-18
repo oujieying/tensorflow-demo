b3245d4c84b4225b43455882a63926ba5b3a56e6
oujieying <ou278136922@gmail.com>
2020-03-18 18:14:57 +0800

    using tensorboard

---
+++ ReadME.md
@@ -0,0 +1,5 @@
+
+## TensorBoard
+```buildoutcfg
+tensorboard --logdir="/Users/jieyingou/anaconda3/envs/tensorflow/tensorflow-demo/logs" --host=127.0.0.1
+```
--- welcome.py
+++ welcome.py
@@ -8,3 +8,18 @@ with tf.Session() as sess:
     print("output: ", sess.run(welcome))
     sess.close()
 
+tf.app.flags.DEFINE_string('log_dir', os.path.dirname(os.path.abspath(__file__)) + '/logs',
+                           'Directory where event logs are written to')
+FLAGS = tf.app.flags.FLAGS
+
+a = tf.constant(5.0, name="a")
+b = tf.constant(10.0, name="b")
+
+x = tf.add(a, b, name="add")
+y = tf.div(a, b, name="div")
+
+with tf.Session() as sess:
+    writer = tf.summary.FileWriter(os.path.expanduser(FLAGS.log_dir), sess.graph)
+    print("Output: ", sess.run([a, b, x, y]))
+    #writer.close()
+    #sess.close()
